b62991ed0036731c74cce518b22885362cd869cd
Natalibro <brodka@mail.ru>
2024-02-26 10:48:10 +0300

    task5

---
+++ task2/task3/task4/task5/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача с прпвильным решением 4
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int variable = number;
+
+while(variable != 0)
+{
+    variable = variable / 10;
+    i *= 10;
+}
+//Console.WriteLine(i);
+
+while(i > 1)
+{
+    i = i/10;
+    variable = number / i;
+    if(i >1){
+        Console.Write(variable + ", ");
+        number = number % i;
+    }
+    else {
+        Console.Write(variable);
+    }
+}
